8080e9359c54464a0c3557bca348bf563d54e31c
maicon <maiconandradexd@gmail.com>
2020-05-02 17:52:23 -0300

    Instalacao e configuracao do nodemon e sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const {Router} = require('express');
+import {Router} from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: "Hello Word" });
+  return res.json({ message: "Hello Alo" });
 })
 
-module.exports = routes;
+export default routes;
